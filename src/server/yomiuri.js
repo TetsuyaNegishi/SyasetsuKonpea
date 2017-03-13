a8e9dc283ed11cd535d6cd2f3014ea91f8ed7741
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-03-13 15:27:57 +0900

    [add]urlとheadlineを配列に格納

--- src/server/yomiuri.js
+++ src/server/yomiuri.js
@@ -3,11 +3,11 @@ import client from 'cheerio-httpcli';
 const yomiuriUrl = 'http://www.yomiuri.co.jp/editorial/';
 
 client.fetch(yomiuriUrl, (err, $) => {
+  const result = [];
   $('ul.list-common > li').each(function () {
     const url = $(this).find('a').attr('href');
     const headline = $(this).find('span.headline').text();
 
-    console.log(`url:${url}`);
-    console.log(`headline:${headline}`);
+    result.push({ url, headline });
   });
 });
