1636de727c3490b1ffe02662d734823aa0652b8b
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-03-13 15:17:47 +0900

    [add]社説のheadlineとurlを取得

--- src/server/yomiuri.js
+++ src/server/yomiuri.js
@@ -3,7 +3,11 @@ import client from 'cheerio-httpcli';
 const yomiuriUrl = 'http://www.yomiuri.co.jp/editorial/';
 
 client.fetch(yomiuriUrl, (err, $) => {
-  $('ul.list-common').each(function () {
-    console.log($(this).text());
+  $('ul.list-common > li').each(function () {
+    const url = $(this).find('a').attr('href');
+    const headline = $(this).find('span.headline').text();
+
+    console.log(`url:${url}`);
+    console.log(`headline:${headline}`);
   });
 });
