44459fd61d32c8d945ce5ff0d8312dcc99521e54
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-03-13 15:29:57 +0900

    [add]urlとheadlineをexport化

--- src/server/yomiuri.js
+++ src/server/yomiuri.js
@@ -2,12 +2,14 @@ import client from 'cheerio-httpcli';
 
 const yomiuriUrl = 'http://www.yomiuri.co.jp/editorial/';
 
-client.fetch(yomiuriUrl, (err, $) => {
-  const result = [];
-  $('ul.list-common > li').each(function () {
-    const url = $(this).find('a').attr('href');
-    const headline = $(this).find('span.headline').text();
+export const getYomiuriList = () => {
+  client.fetch(yomiuriUrl, (err, $) => {
+    const result = [];
+    $('ul.list-common > li').each(function () {
+      const url = $(this).find('a').attr('href');
+      const headline = $(this).find('span.headline').text();
 
-    result.push({ url, headline });
+      result.push({ url, headline });
+    });
   });
-});
+};
