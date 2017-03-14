b89f9fa7095304957e6bcfd1ac1350068967a95b
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-03-14 21:52:48 +0900

    [add]yomiuri.jsをwebAPI化

--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 import express from 'express';
+import { getYomiuriOpinions, getYomiuriOpinion } from './server/yomiuri';
 
 const app = express();
 
 // HTTPリクエストを受け取る部分
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/yomiuri', (req, res) => {
+  const url = req.query.url;
+  if (url) {
+    getYomiuriOpinion(url).then((data) => {
+      res.json({ result: 'OK', data });
+    }).catch((error) => {
+      res.json({ result: 'NG', error });
+    });
+  } else {
+    getYomiuriOpinions().then((data) => {
+      res.json({ result: 'OK', data });
+    }).catch((error) => {
+      res.json({ result: 'NG', error });
+    });
+  }
 });
 
 // サーバーを起動する部分
