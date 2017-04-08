0ae6579ef329b04b719c69a8254a6ea560ed642c
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-04-08 15:02:54 +0900

    [update]bootstrapを導入

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,9 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 fetch('/yomiuri').then(res => console.log(res));
 
